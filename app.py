66c2a25291336b1c25545d364e7cd69946357f7d
yyyyqqqqq <962800840@qq.com>
2020-01-19 13:59:00 +0800

    yq

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.register_blueprint(pf_blue)
 #
 @app.route('/')
 def index():
-    print('1234455667788')
+    print('11')
     return render_template("index.html")
 # #
 @app.errorhandler(404)
