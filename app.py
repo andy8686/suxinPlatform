afa2e21a734f87f4dd523b87926b3059c0313906
andy8686 <13085001100@qq.com>
2020-01-19 12:39:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app.register_blueprint(pf_blue)
 #     # return jsonify(out_param)
 #     pass
 #
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
 # #
 @app.errorhandler(404)
 def err_404_handler(err):
